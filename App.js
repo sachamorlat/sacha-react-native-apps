542afcf57e5d406b6163f8323cfb89ca2187c4da
unknown <devlyon25@academy.priv>
2023-12-08 13:35:35 +0100

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,KeyboardAvoidingView, Text, View, FlatList, TextInput, TouchableOpacity, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Liste d'Objectif</Text>
+        {/* <KeyboardAvoidingView
+        style = {{ flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}> */}
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Nouvel objectif..."
+            onChangeText={(text) => setNewGoal(text)}
+          />
+          <TouchableOpacity style={styles.addButton}>
+            <Text style={styles.addButtonText} onPress={() => addGoal()}>Add</Text>
+          </TouchableOpacity>
+        </View>
+      {/* </KeyboardAvoidingView> */}
+      <FlatList
+        data={sampleGoals}        
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +38,77 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 5,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    borderRadius: 5,
+  },
+  addButton: {
+    backgroundColor: '#08F',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  addButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  goalItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  goalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
+
+let sampleGoals = [
+  { id: '1', title: 'Objectif 1', description: 'Faire les courses' },
+  { id: '2', title: 'Objectif 2', description: 'Aller à la salle de sport 3 fois par semaine' },
+  { id: '3', title: 'Objectif 3', description: 'Monter à plus de 5000m d altitude' },
+  { id: '4', title: 'Objectif 4', description: 'Acheter mon premier appartement' },
+  { id: '5', title: 'Objectif 5', description: 'Perdre 5 kgs' },
+  { id: '6', title: 'Objectif 6', description: 'Gagner en productivité' },
+  { id: '7', title: 'Objectif 7', description: 'Apprendre un nouveau langage' },
+  { id: '8', title: 'Objectif 8', description: 'Faire une mission en freelance' },
+  { id: '9', title: 'Objectif 9', description: 'Organiser un meetup autour de la tech' },
+  { id: '10', title: 'Objectif 10', description: 'Faire un triathlon' },
+];
+
+const renderItem = ({ item }) => (
+  <View style={{padding: 16, borderBottomWidth: 1, borderBottomColor: '#ccc' }}>
+    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{item.title}</Text>
+    <Text>{item.description}</Text>
+  </View>
+);
+
+let newGoal = '';
+
+
+const setNewGoal = (text) => {
+  newGoal = text;
+};
+
+const addGoal = () => {
+  if (newGoal.trim() === '') {
+    return;
+  }
+  const newId = (parseInt(sampleGoals[sampleGoals.length - 1].id) + 1).toString();
+  const newGoalItem = { id: newId, title: `Objectif ${newId}`, description: newGoal };
+  sampleGoals = [...sampleGoals, newGoalItem];
+
+  newGoal = '';
+  return sampleGoals;
+};
